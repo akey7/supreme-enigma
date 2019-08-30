150b2ccd5f496d144f3bd069ec87827b4c351b0d
Alicia Key <alicia.key@nrel.gov>
2019-08-29 22:38:00 -0600

    Issue #3
    - Make a different string to trigger a PR

--- test_StringMe.py
+++ test_StringMe.py
@@ -3,7 +3,7 @@ from StringMe import StringMe
 
 class TestStringMe(unittest.TestCase):
     def test_reflect(self):
-        expected = "alicia"
+        expected = "hello"
         instance = StringMe(expected)
         actual = instance.reflect()
         self.assertEqual(expected, actual)
